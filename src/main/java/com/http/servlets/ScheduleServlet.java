bbc9ff1f5ad8360b3449b8f3db39756f8bf71835
igoririsproject <igor.zak.89@gmail.com>
2021-12-27 12:48:50 +0200

    update

--- src/main/java/com/http/servlets/ScheduleServlet.java
+++ src/main/java/com/http/servlets/ScheduleServlet.java
@@ -6,12 +6,12 @@ import java.io.InputStreamReader;
 import java.util.concurrent.RunnableScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
 
